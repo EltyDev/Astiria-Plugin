c3d84054cc48681fa5ddf1b934664a797fe50755
Venodez <Venodez@users.noreply.github.com>
2020-01-27 17:11:10 +0100

    Fix commande /aban

--- src/fr/venodez/plugin/CommandBan.java
+++ src/fr/venodez/plugin/CommandBan.java
@@ -1,15 +1,17 @@
 package fr.venodez.plugin;
 
 import org.bukkit.BanList;
-import org.bukkit.BanList.Type;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class CommandBan implements CommandExecutor {
-
+	
+	public String raison;
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
 		
@@ -18,7 +20,6 @@ public class CommandBan implements CommandExecutor {
 			if (sender instanceof Player) {
 				
 				Player player = (Player) sender;
-				Player target = Bukkit.getServer().getPlayer(args[0]);
 				if (args.length == 0) {
 					
 					player.sendMessage("§cIl faut donner un joueur");
@@ -29,25 +30,57 @@ public class CommandBan implements CommandExecutor {
 					player.sendMessage("§cIl faut donner une raison");
 				}
 				
-				else if (args.length >= 2) {
-					
+				else if (args.length >= 2) {	
+
+					Player target = Bukkit.getServer().getPlayer(args[0]);
 					target.getWorld().strikeLightning(target.getLocation());
-					String raison = null;
 					for (int i = 1; i == args.length; i++) {
 						
 						raison += args[i];
 							
 					}
+					
+					
+					String pMessage = "§cVous avez été bannis du serveur" + "\n" + "§7Raison : §f" + raison + "\n" + "§7Par : §f" + player.getName();
 					Bukkit.getServer().getBanList(BanList.Type.NAME).addBan(args[0], raison, null, null);
-					String message = "§e§l[Modération] §r§c" + args[0] + " §6s'est fait ban par §5§l" + player.getName() + " §r§6pour §c" + raison;
-					Bukkit.broadcastMessage(String.format(message));
+					target.kickPlayer(pMessage);
+					String bMessage = "§e§l[Modération] §r§c" + args[0] + " §6s'est fait ban par §5§l" + player.getName() + " §r§6pour §c" + raison;
+					Bukkit.broadcastMessage(String.format(bMessage));
 					
 				} 
 			}
 			
 			else {
 				
+				ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+				if (args.length == 0) {
+					
+					console.sendMessage("§cIl faut donner un joueur");
+				}
 				
+				else if (args.length == 1) {
+					
+					console.sendMessage("§cIl faut donner une raison");
+				}
+				
+				else if (args.length >= 2) {	
+					
+					Player target = Bukkit.getServer().getPlayer(args[0]);
+					target.getWorld().strikeLightning(target.getLocation());
+					for (int i = 1; i == args.length; i++) {
+						
+						raison += args[i];
+							
+					}
+					
+					String pMessage = "§cVous avez été bannis du serveur" + "\n" + "§7Raison : §f" + raison + "\n" + "§7Par :§f Console";
+					Bukkit.getServer().getBanList(BanList.Type.NAME).addBan(args[0], raison, null, null);
+					target.kickPlayer(pMessage);
+					String message = "§e§l[Modération] §r§c" + args[0] + " §6s'est fait ban par la §5§lconsole §r§6pour §c" + raison;
+					Bukkit.broadcastMessage(String.format(message));
+
+				}
+			
 			}
 		
 		}
